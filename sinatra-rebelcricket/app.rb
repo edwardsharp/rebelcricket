82f0c7edbe5170b233f45ba93cc7e3ba8c379085
edward sharp <edward@edwardsharp.net>
2016-10-17 14:31:44 -0700

    cors

--- sinatra-rebelcricket/app.rb
+++ sinatra-rebelcricket/app.rb
@@ -5,12 +5,33 @@ Bundler.require
 
 require 'mailgun'
 
+require 'sinatra/cross_origin'
+register Sinatra::CrossOrigin
+configure do
+  enable :cross_origin
+end
+
 Dotenv.load
 
 # Setup DataMapper with a database URL. On Heroku, ENV['DATABASE_URL'] will be
 # set, when working locally this line will fall back to using SQLite in the
 # current directory.
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/rebelcricket')
+
+#cors
+# set :protection, origin_whitelist: ['http://localhost:8080']
+set :allow_origin, ['http://localhost:8080', 'http://rebelcricket.lacuna.club', 'http://rebelcricket.com']
+set :allow_methods, [:post]
+set :allow_credentials, false #not using http auth or cookies
+set :max_age, "1728000" #20 dayz
+set :expose_headers, ['Content-Type']
+# options "*" do
+#   response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
+#   response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+#   200
+# end
+
+enable :logging, :dump_errors, :raise_errors
 
 # Define a simple DataMapper model.
 class RebelContact
@@ -29,11 +50,12 @@ class RebelContact
   end
 
   def send_message
+    return if self.message_sent
     # First, instantiate the Mailgun Client with your API key
     mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
 
     # Define your message parameters
-    message_params =  { from: 'Rebel Contact <postmaster@sandboxd9b8d0a54b1549c89a948403df95088d.mailgun.org>',
+    message_params =  { from: 'Rebel Contact <noreply@mg.lacuna.club>',
                         to:   ENV['MAIL_GOES_TO'],
                         subject: "#{self.email} #{self.subject}",
                         text:    self.body
@@ -43,6 +65,7 @@ class RebelContact
     begin
       mg_client.send_message ENV['MAILGUN_DOMAIN'], message_params
       self.message_sent = true
+      p "MESSAGE SENT!"
       save_self
     rescue 
       p "COULD NOT SEND MESSAGE!"
@@ -62,37 +85,27 @@ DataMapper.finalize
 # Tell DataMapper to update the database according to the definitions above.
 DataMapper.auto_upgrade!
 
-get '/' do
-  if ENV['SERVE_TEST_INDEX'] == 'true'
+if ENV['SERVE_TEST_INDEX'] == 'true'
+  get '/' do
     send_file './rebel_contact.html'
-  else
-    'rebel contact'
   end
-  
 end
 
 # # Route to show all RebelContacts, ordered like a blog
 # get '/rebel_contacts' do
 #   content_type :json
 #   @rebel_contacts = RebelContact.all(order: :created_at.desc)
-
 #   @rebel_contacts.to_json
 # end
 
-# CREATE: Route to create a new RebelContact
 post '/new' do
   content_type :json
 
-  # These next commented lines are for if you are using Backbone.js
-  # JSON is sent in the body of the http request. We need to parse the body
-  # from a string into JSON
   # params_json = JSON.parse(request.body.read)
-
-  # If you are using jQuery's ajax functions, the data goes through in the
-  # params.
   p "params: #{params}"
 
   @rebel_contact = RebelContact.new(params)
+  @rebel_contact.created_at = Time.now
 
   if @rebel_contact.save
     @rebel_contact.to_json
@@ -101,11 +114,9 @@ post '/new' do
   end
 end
 
-# # READ: Route to show a specific RebelContact based on its `id`
 # get '/rebel_contacts/:id' do
 #   content_type :json
 #   @rebel_contact = RebelContact.get(params[:id].to_i)
-
 #   if @rebel_contact
 #     @rebel_contact.to_json
 #   else
@@ -113,21 +124,11 @@ end
 #   end
 # end
 
-# # UPDATE: Route to update a RebelContact
 # put '/rebel_contacts/:id' do
 #   content_type :json
-
-#   # These next commented lines are for if you are using Backbone.js
-#   # JSON is sent in the body of the http request. We need to parse the body
-#   # from a string into JSON
 #   # params_json = JSON.parse(request.body.read)
-
-#   # If you are using jQuery's ajax functions, the data goes through in the
-#   # params.
-
 #   @rebel_contact = RebelContact.get(params[:id].to_i)
 #   @rebel_contact.update(params)
-
 #   if @rebel_contact.save
 #     @rebel_contact.to_json
 #   else
@@ -135,11 +136,9 @@ end
 #   end
 # end
 
-# # DELETE: Route to delete a RebelContact
 # delete '/rebel_contacts/:id/delete' do
 #   content_type :json
 #   @rebel_contact = RebelContact.get(params[:id].to_i)
-
 #   if @rebel_contact.destroy
 #     {:success => "ok"}.to_json
 #   else
