8dc2c973d42a0c5361fc27d8565aa8a1975d0519
edward sharp <edward@edwardsharp.net>
2018-08-28 01:32:57 -0400

    fix init script :feelsgood:

--- docker_util/rebelcricket/init-user-db.sh
+++ docker_util/rebelcricket/init-user-db.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER rebelcricket WITH PASSWORD 'zYB27Bzwu54yNVgWe';
+    CREATE USER rebelcricket WITH PASSWORD "$POSTGRES_PASSWORD";
     CREATE DATABASE rebelcricket_production;
     GRANT ALL PRIVILEGES ON DATABASE rebelcricket_production TO rebelcricket;
     CREATE USER kan WITH PASSWORD 'board';
